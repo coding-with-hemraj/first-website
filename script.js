e581e7b7926384189cef66bac742bca7908b1b05
Hemraj <hemrajkumar.1807@gmail.com>
2026-09-09 19:00:43 +0530

    Create professional landing page

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function showMessage() {
-    alert("Bhai, JavaScript chal raha hai! 🚀");
-}
+const contactButton = document.getElementById("contactButton");
+
+const helloButton = document.getElementById("helloButton");
+
+
+contactButton.addEventListener("click", function () {
+
+    document.getElementById("contact").scrollIntoView({
+        behavior: "smooth"
+    });
+
+});
+
+
+helloButton.addEventListener("click", function () {
+
+    alert("Hello bhai! 👋 Thanks for visiting my website.");
+
+});
